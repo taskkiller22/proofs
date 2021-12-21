198cfb28a8f14bc92b6f1ec8846b1e020c0f659d
Ricardo Suarez <ricardo.suarez@wnco.com>
2021-12-21 13:47:29 -0500

    first commit intellij project

---
+++ src/main/java/com/test/proofs/ProofsApplication.java
@@ -0,0 +1,13 @@
+package com.test.proofs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProofsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProofsApplication.class, args);
+    }
+
+}
